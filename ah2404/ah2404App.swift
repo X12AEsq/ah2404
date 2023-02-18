e80ffe63b7afdf03e1350714ecb6ca4212ec4167
Morris Albers <dagger5@alberslegal.com>
2023-02-18 10:22:37 -0600

    Initial Commit

---
+++ ah2404/ah2404App.swift
@@ -0,0 +1,17 @@
+//
+//  ah2404App.swift
+//  ah2404
+//
+//  Created by Morris Albers on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ah2404App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
